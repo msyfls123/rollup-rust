1e44f59856c76012d4269110462d0a9d586e1338
msyfls123 <msyfls123@gmail.com>
2021-02-12 16:32:34 +0800

    use rollup-plugin-typescript2

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import typescript from '@rollup/plugin-typescript'
+import typescript from 'rollup-plugin-typescript2'
 import html from '@rollup/plugin-html'
 import serve from 'rollup-plugin-serve'
 import livereload from 'rollup-plugin-livereload'
